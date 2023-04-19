3aee7ba5c7a7a00a6bb10b3e8272171d68bb7582
Bach906 <70774853+Bach906@users.noreply.github.com>
2023-04-19 09:52:20 -0300

    corrigi prob simple_LienarSearch, add teste no final pra comparar result e cleanup geral do codigo

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import math
 from random import randint
 
-#ta tudo errado, falta tirar m 
-
 #global var
 distr_flag = 0
 
@@ -17,11 +15,11 @@ def bubblesort(list):
                 list[idx+1] = temp
     return list
 
+
 def get_median(a):
     bubblesort(a)
     index = math.ceil(len(a)/2) #round up
     return a[index - 1]
-
 
 
 def simple_LinearSelection(A, k): #visto em sala
@@ -32,7 +30,7 @@ def simple_LinearSelection(A, k): #visto em sala
 
     if len(A) <= 1:
         return get_median(A)
-    
+
     for i in A:
         len_5_list.append(i)
         aux += 1
@@ -41,9 +39,9 @@ def simple_LinearSelection(A, k): #visto em sala
             median_list.append(get_median(len_5_list))
             len_5_list.clear()
 
-    m = LinearSelection(median_list, math.ceil(len(median_list)/2))
+    m = simple_LinearSelection(median_list, math.ceil(len(median_list)/2))
     A.remove(m)
-    
+
     R = []
     L = []
 
@@ -53,14 +51,13 @@ def simple_LinearSelection(A, k): #visto em sala
         elif i > m:
             R.append(i)
 
-
     if k == len(L) + 1:
         return m
     elif k <= len(L):
-        return LinearSelection(L, k)
+        return simple_LinearSelection(L, k)
     else: #elif k > len(L):
-        return LinearSelection(R, k - len(L) - 1)
-    
+        return simple_LinearSelection(R, k - len(L) - 1)
+
 
 def LinearSelection(A, k): # precisa funcionar com floats repetidos
     median_list = []
@@ -70,7 +67,7 @@ def LinearSelection(A, k): # precisa funcionar com floats repetidos
 
     if len(A) <= 1:
         return get_median(A)
-    
+
     for i in A:
         len_5_list.append(i)
         aux += 1
@@ -81,7 +78,7 @@ def LinearSelection(A, k): # precisa funcionar com floats repetidos
 
     m = LinearSelection(median_list, math.ceil(len(median_list)/2))
     A.remove(m)
-    
+
     R = []
     L = []
 
@@ -93,32 +90,30 @@ def LinearSelection(A, k): # precisa funcionar com floats repetidos
             R.append(i)
             distr_flag = 0
 
-
     if k == len(L) + 1:
         return m
     elif k <= len(L):
         return LinearSelection(L, k)
     else: #elif k > len(L):
         return LinearSelection(R, k - len(L) - 1)
-    
 
 
 def main():
     A = []
     k = 20
 
-    # while(len(A) < 100):
-    #     i = randint(1, 100)
-    #     print(i, end = ", ")
-    #     A.append(i)
-    
+    while(len(A) < 100):
+        i = randint(1, 100)
+        A.append(i)
 
-    
-    median_of_medians = LinearSelection(A, k)
+    result = LinearSelection(A, k)
+
     A.sort()
-    for i in A:
-        print(i, end = ", ")
-    print(median_of_medians)
+    for i in range(len(A) - 1):
+        print(A[i], end = ",")
+    
+    print()
+    print("k'esimo menor valor =", A[k-1], "\nresultado da funcao =", result)
 
     return
 
